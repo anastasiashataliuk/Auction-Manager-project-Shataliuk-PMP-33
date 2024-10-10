273b6ae8e07fa8d158f086217713bbda8c3d6d2c
anastasiashataliuk <anastasiya.shatalyk@gmail.com>
2024-10-10 23:39:32 +0300

    Add files via upload

---
+++ DALL/Interface/IProductDAL.cs
@@ -0,0 +1,13 @@
+﻿using DTO;
+
+namespace DAL.Interface
+{
+    public interface IProductDal
+    {
+        List<Product> GetAll(); // Отримати всі продукти
+        Product GetById(int id); // Отримати продукт за ID
+        Product Insert(Product product); // Вставити новий продукт
+        void Update(Product product); // Оновити існуючий продукт
+        void Delete(int id); // Видалити продукт за ID
+    }
+}
